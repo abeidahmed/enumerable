877a5c59d8a7c04c82a536a47b57001790efaa4c
abeidahmed <abeidahmed92@gmail.com>
2020-12-30 14:39:13 +0530

    Refactor test

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,32 +1,32 @@
 require 'rspec'
 require_relative '../lib/enumerable'
 
-describe "#my_each" do
+RSpec.describe Enumerable do
   let(:arr) { [1, 2, 3] }
   let(:range) { (1..5) }
   let(:string_arr) { %w[john doe man] }
 
-  it "should return the same array" do
-    expect(arr.my_each { |ele| ele }).to match_array(arr)
+  describe "#my_each" do
+    it "should return the same array" do
+      expect(arr.my_each { |ele| ele }).to match_array(arr)
+    end
+
+    it "should iterate a range and return the same array" do
+      expect(range.my_each { |ele| ele }).to match_array(range)
+    end
+
+    it "should iterate a string array and return the same array" do
+      expect(string_arr.my_each { |ele| ele }).to match_array(string_arr)
+    end
   end
 
-  it "should iterate a range and return the same array" do
-    expect(range.my_each { |ele| ele }).to match_array(range)
-  end
+  describe "#my_select" do
+    it "should filter the array and return appropriate result" do
+      expect(arr.my_select { |n| n > 2 }).to match_array([3])
+    end
 
-  it "should iterate a string array and return the same array" do
-    expect(string_arr.my_each { |ele| ele }).to match_array(string_arr)
-  end
-end
-
-describe "#my_select" do
-  let(:arr) { [1, 2, 3] }
-
-  it "should filter the array and return appropriate result" do
-    expect(arr.my_select { |n| n > 2 }).to match_array([3])
-  end
-
-  it "should return an Enumerable if no block is given" do
-    expect(arr.my_select).to be_a(Enumerable)
+    it "should return an Enumerable if no block is given" do
+      expect(arr.my_select).to be_a(Enumerable)
+    end
   end
 end
