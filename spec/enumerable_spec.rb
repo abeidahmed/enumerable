84ed432de2a99d5decd7dd3dff676e44a5553f9d
abeidahmed <abeidahmed92@gmail.com>
2020-12-30 14:37:47 +0530

    Add my_each test

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -3,9 +3,19 @@ require_relative '../lib/enumerable'
 
 describe "#my_each" do
   let(:arr) { [1, 2, 3] }
+  let(:range) { (1..5) }
+  let(:string_arr) { %w[john doe man] }
 
   it "should return the same array" do
     expect(arr.my_each { |ele| ele }).to match_array(arr)
+  end
+
+  it "should iterate a range and return the same array" do
+    expect(range.my_each { |ele| ele }).to match_array(range)
+  end
+
+  it "should iterate a string array and return the same array" do
+    expect(string_arr.my_each { |ele| ele }).to match_array(string_arr)
   end
 end
 
